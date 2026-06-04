ae4dd597cce24675858b7952f240456dd782777a
Nikstylx <201nick.h@gmail.com>
2026-06-04 19:53:07 -0400

    Final Working

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
+
+const userRoutes = require('./server/routes/user');
 
 app.use(express.json());
 
+// CORS (from professor requirement)
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, Options");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 
+// Static files
 app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
+// Routes
+app.use('/users', userRoutes);
+
+// Home route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
